72fca7f581da3a134707bd8149b942d96bcaabef
Mariela Aguirre <marielaaguirre610@gmail.com>
2026-07-08 00:57:56 -0300

    img clin

--- src/constants/ImgProjects/ImgClinic.js
+++ src/constants/ImgProjects/ImgClinic.js
@@ -1,3 +1,3 @@
 export const ImgClinic = [ 
-    {id:1, src:'./projects/Clinic/Clinic.png', alt:"Indeed"},
+    {id:1, src:'./projects/Clinic/Clinic.png', alt:"Imagen de código de proyecto, con React, Supabase, TailwindCSS, PostrgeSQL, Docker. Numero 1. "},
 ];

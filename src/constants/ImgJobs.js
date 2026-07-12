71ddea386b40174a6e3619c6141adcc1237c7e27
Mariela Aguirre <marielaaguirre610@gmail.com>
2026-07-12 20:50:07 -0300

    guardar

--- src/constants/ImgJobs.js
+++ src/constants/ImgJobs.js
@@ -2,5 +2,5 @@ export const ImgJobs =[
     {id:1, src:"./Social_Job/indeed.svg", alt:"Indeed", url:"https://profile.indeed.com/?hl=es_AR&co=AR&from=gnav-passport--passport-webapp"},
     {id:2, src:"./Social_Job/infojobs-logo.svg", alt:"Infojobs", url:"https://www.infojobs.net/candidate/cv/view/index.xhtml"},
     {id:3, src:"./Social_Job/jobicy.svg", alt:"Jobicy", url:"https://jobicy.com/u/id70164"},
-    {id:4, src:"./Social_Job/linkedin.svg", alt:"Linkedin", url:"www.linkedin.com/in/mariela-aguirre-80a607309"},
+    {id:4, src:"./Social_Job/linkedin.svg", alt:"Linkedin", url:"https://www.linkedin.com/in/mariela-aguirre-80a607309/"},
 ];
